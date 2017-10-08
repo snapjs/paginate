0197e2e3cb042f316206316357d42eaae8880ff1
Nick Scialli <snapjavascript@gmail.com>
2017-10-07 20:23:46 -0400

    Fix pager click bug

--- paginate.js
+++ paginate.js
@@ -1,15 +1,4 @@
 'use strict'
-function showPage(page) {
-	content.innerHTML = '';
-	for (var i = 0; i < elements[page].length; i++) {
-		content.appendChild(elements[page][i]);
-	}
-	for (var i = 0; i < document.getElementsByClassName('pager-link').length; i++) {
-		document.getElementsByClassName('pager-link')[i].setAttribute('class','pager-link');
-	}
-	document.getElementById('pager-link-'+page).setAttribute('class','pager-link pager-link-selected')
-	return false;
-}
 function paginate(contentId,pagerId,perPage) {
 	var content = document.getElementById(contentId);
 	var k = 0;
@@ -17,12 +6,23 @@ function paginate(contentId,pagerId,perPage) {
 	window.elements = {};
 	function createPageLink(page) {
 		var pagerLink = document.createElement('a');
-		pagerLink.setAttribute('onclick','showPage('+page+')');
+
 		pagerLink.setAttribute('href','#');
 		pagerLink.setAttribute('id','pager-link-'+page)
 		pagerLink.setAttribute('class','pager-link')
 		pagerLink.innerHTML = page;
 		document.getElementById(pagerId).appendChild(pagerLink);
+		document.getElementById('pager-link-'+page).onclick = function() {
+			content.innerHTML = '';
+			for (var i = 0; i < elements[page].length; i++) {
+				content.appendChild(elements[page][i]);
+			}
+			for (var i = 0; i < document.getElementsByClassName('pager-link').length; i++) {
+				document.getElementsByClassName('pager-link')[i].setAttribute('class','pager-link');
+			}
+			document.getElementById('pager-link-'+page).setAttribute('class','pager-link pager-link-selected')
+			return false;
+		}
 	}
 	createPageLink(1);
 	for (var i = 0; i < content.childNodes.length; i++) {
@@ -40,6 +40,16 @@ function paginate(contentId,pagerId,perPage) {
 	if (page === 1) {
 		document.getElementById(pagerId).innerHTML = '';
 	}
-	showPage(1);
+
+	content.innerHTML = '';
+	for (var i = 0; i < elements[1].length; i++) {
+		content.appendChild(elements[1][i]);
+	}
+	for (var i = 0; i < document.getElementsByClassName('pager-link').length; i++) {
+		document.getElementsByClassName('pager-link')[i].setAttribute('class','pager-link');
+	}
+	document.getElementById('pager-link-1').setAttribute('class','pager-link pager-link-selected')
+	return false;
+
 }
 module.exports = paginate;
